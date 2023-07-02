7ebdd563321a555ce28ef936028297349421be44
Igor Komlew <igor.komlew@thunes.com>
2023-07-02 13:12:30 +0200

    Test backend pipeline

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const message = "Hello, World 2!"
+const message = "Hello, World 3!"
 
 // ResponseData represents the structure of the response data
 type ResponseData struct {
